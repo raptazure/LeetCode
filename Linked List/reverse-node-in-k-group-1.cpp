7b3788d5fd9309864d4fbc510cb199bced24a7b6
raptazure <raptazure@foxmail.com>
2020-12-06 23:36:02 +0800

    update

--- Linked List/reverse-node-in-k-group-1.cpp
+++ Linked List/reverse-node-in-k-group-1.cpp
@@ -58,19 +58,15 @@ public:
     {
         if (head == NULL)
             return NULL;
-        // 区间 [a, b) 包含 k 个待反转元素
         ListNode *a = head;
         ListNode *b = head;
         for (int i = 0; i < k; i++)
         {
-            // 不足 k 个，不需要反转，base case
             if (b == NULL)
                 return head;
             b = b->next;
         }
-        // 反转前 k 个元素
         ListNode *newHead = reverse(a, b);
-        // 递归反转后续链表并连接起来
         a->next = reverseKGroup(b, k);
         return newHead;
     }
